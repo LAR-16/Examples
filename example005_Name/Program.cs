06fbb67b9d4e6efd3c6a4a6000cdacae8ca26036
Alexandra <lebedeva1618@yandex.ru>
2022-12-07 20:43:39 +0300

    добавили знак вопроса

--- example005_Name/Program.cs
+++ example005_Name/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Введите имя пользователя");
-string username = Console.ReadLine();
+﻿Console.Write("Введите имя пользователя ");
+string? username = Console.ReadLine();
 
 if(username.ToLower() == "маша")
 {
