c9b2ef84b42a719598373a14a0b2e1511da054b7
Alexandra <lebedeva1618@yandex.ru>
2022-12-07 21:44:03 +0300

    упорядочивание массива от максимального к минимальному

---
+++ example015_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿// упорядочить массив от максимального к минимальному
+
+Console.Clear();
+int[] arr = {13, 5, 4, 3, 2, 6, 7, 21, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+        {
+            int maxPosition = i;
+            for (int j = i+1; j < array.Length; j++)
+                {
+                    if(array[j] > array[maxPosition]) maxPosition = j;
+                        
+                }
+                int temporary = array[i];
+                array[i] = array[maxPosition];
+                array[maxPosition] = temporary;
+        }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
